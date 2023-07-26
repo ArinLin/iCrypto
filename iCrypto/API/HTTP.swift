54aa658f2c50d84283d0b9fa241e7ff698b4ac59
Arina <arinaligit@gmail.com>
2023-07-26 17:38:15 +0300

    add API and Endpoints

---
+++ iCrypto/API/HTTP.swift
@@ -0,0 +1,26 @@
+//
+//  HTTP.swift
+//  iCrypto
+//
+//  Created by Arina on 26.07.2023.
+//
+
+import Foundation
+
+enum HTTP {
+    enum Method: String {
+        case get = "GET"
+        case post = "POST"
+    }
+    
+    enum Headers {
+        enum Key: String {
+            case contentType = "Content-Type"
+            case apiKey = "X-CMC_PRO_API_KEY"
+        }
+        
+        enum Value: String {
+            case applicationJson = "application/json"
+        }
+    }
+}
--- iCrypto/Models/CoinError.swift
+++ iCrypto/Models/CoinError.swift
@@ -7,20 +7,6 @@
 
 import Foundation
 
-//struct CoinStatus: Decodable {
-//    let status: CoinError
-//}
-//
-//struct CoinError: Decodable {
-//    let errorCode: Int
-//    let errorMessage: String
-//
-//    enum CodingKeys: String, CodingKey {
-//        case errorCode = "error_code"
-//        case errorMessage = "error_message"
-//    }
-//}
-
 struct CoinError: Decodable {
     let errorCode: Int
     let errorMessage: String
